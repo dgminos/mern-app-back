a9491b266852420c9110094f26100d2381abda9d
Diana Gabriela Miños <eurekagaudeamus@gmail.com>
2022-11-17 16:29:28 -0300

    Fix file path to import user model

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,4 +1,5 @@
-import User  from '../models/UserModel.js'
+//import User  from '../models/UserModel.js'
+import User  from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 
 export const requireAuth = async (req, res, next) => {
